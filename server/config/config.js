1cf673fa8d57c291db5864a6bbc51ad85392d1b2
1000pilar <nunua7a8@gmail.com>
2017-11-24 19:16:29 +0700

    port to 80

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 console.log(process.env.PASSWORD);
 var config = {
-  port: process.env.PORT || 3002,
+  port: process.env.PORT || 80,
   test_port: 3001,
   mongoURI: {
     development: "mongodb://nugraha:nunuhana180390!@facedetect-shard-00-00-gjnar.mongodb.net:27017,facedetect-shard-00-01-gjnar.mongodb.net:27017,facedetect-shard-00-02-gjnar.mongodb.net:27017/Fareat?ssl=true&replicaSet=FaceDetect-shard-0&authSource=admin",
